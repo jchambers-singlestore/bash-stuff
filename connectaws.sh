7614a7fb14785d4ddd83fb3537480929c8679bbf
jchambers-singlestore <108073355+jchambers-singlestore@users.noreply.github.com>
2022-07-27 16:12:40 -0400

    Update connectaws.sh

--- connectaws.sh
+++ connectaws.sh
@@ -2,13 +2,26 @@
 # This script will pull your aws token info from your $HOME/Downloads/credentials file and print your instance IPs.
 # It will make an SSH connection to the instance you choose.
 
-AWS_ACCESS_KEY_ID=$(cat $HOME/Downloads/credentials | cut -d " " -f 3 | grep -v default | sed -n 1p)
-AWS_SECRET_ACCESS_KEY=$(cat $HOME/Downloads/credentials | cut -d " " -f 3 | grep -v default | sed -n 2p)
-AWS_SESSION_TOKEN=$(cat $HOME/Downloads/credentials | cut -d " " -f 3 | grep -v default | sed -n 3p)
+export AWS_ACCESS_KEY_ID=$(cat $HOME/Downloads/credentials | cut -d " " -f 3 | grep -v default | sed -n 1p)
+export AWS_SECRET_ACCESS_KEY=$(cat $HOME/Downloads/credentials | cut -d " " -f 3 | grep -v default | sed -n 2p)
+export AWS_SESSION_TOKEN=$(cat $HOME/Downloads/credentials | cut -d " " -f 3 | grep -v default | sed -n 3p)
 
+export i1=i-04c304083f3b98df2
+export i2=i-0449445745458f2fd
+
+state1=`aws ec2 describe-instances --instance-ids $i1 | grep STATE | awk '{print $3}'`
+state2=`aws ec2 describe-instances --instance-ids $i2 | grep STATE | awk '{print $3}'`
+
+## Ensure instances are running
+if [[ "$state1" == 'running' ]] && [[ "$state2" == 'running' ]] ; then
+	echo "Instance are already running. Skipping startup"
+else
+	aws ec2 start-instances --instance-ids $i1 $i2
+fi
+
+## Connection Stuff
 echo "Your instances:"
 my_array=(`aws ec2 describe-instances --filters "Name=tag-value,Values=jchambers" | grep ASSOCIATION | awk '{print $4}' | uniq`)
-
 i=0
 x=1
 while [ $i -lt ${#my_array[@]} ]
@@ -19,13 +32,12 @@ do
 done
 
 read -p 'Choose a server: ' server
-
 re='^[0-9]+$'
+
 if ! [[ $server =~ $re ]] ; then
    echo "error: Not a number" >&2; exit 1
 fi
-
-ssh -i $HOME/.ssh/yourpemfile.pem ubuntu@`echo ${my_array[$server - 1]}`
+ssh -i $HOME/.ssh/joeskeyaws_ohio.pem ubuntu@`echo ${my_array[$server - 1]}`
 
 # Testing stuff
 #echo ${#my_array[@]}
