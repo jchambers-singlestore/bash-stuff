097c8250e5c704105b4509109553a7440a732700
jchambers-singlestore <108073355+jchambers-singlestore@users.noreply.github.com>
2023-01-12 06:55:00 -0500

    Update connectaws.sh
    Added checks for credentials
    Added check for instance running status before SSHing
    Added SSH using ec2-user instead of only ubuntu

--- connectaws.sh
+++ connectaws.sh
@@ -1,50 +1,83 @@
 #!/bin/bash
-# This script will pull your aws token info from your $HOME/Downloads/credentials file and print your instance IPs.
-# It will make an SSH connection to the instance you choose.
 
-export AWS_ACCESS_KEY_ID=$(cat $HOME/Downloads/credentials | cut -d " " -f 3 | grep -v default | sed -n 1p)
-export AWS_SECRET_ACCESS_KEY=$(cat $HOME/Downloads/credentials | cut -d " " -f 3 | grep -v default | sed -n 2p)
-export AWS_SESSION_TOKEN=$(cat $HOME/Downloads/credentials | cut -d " " -f 3 | grep -v default | sed -n 3p)
-
-export i1=i-04c304083f3b98df2
-export i2=i-0449445745458f2fd
-
-state1=`aws ec2 describe-instances --instance-ids $i1 | grep STATE | awk '{print $3}'`
-state2=`aws ec2 describe-instances --instance-ids $i2 | grep STATE | awk '{print $3}'`
-
-#TAGS1=`aws ec2 describe-instances --instance-ids i1 | grep TAGS`
-#TAGS2=`aws ec2 describe-instances --instance-ids i2 | grep TAGS`
-
-## Ensure instances are running
-if [[ "$state1" == 'running' ]] && [[ "$state2" == 'running' ]] ; then
-	echo "Instance are already running. Skipping startup"
-else
-	aws ec2 start-instances --instance-ids $i1 $i2
-	echo "Starting instances, waiting 12s."
-	sleep 12
+#check if the credentials file exists
+if [ ! -f ~/Downloads/credentials ]; then
+    echo "Credentials file not found!"
+    exit 1
 fi
 
-## Connection Stuff
-echo "Your instances:"
-my_array=(`aws ec2 describe-instances --filters "Name=tag-value,Values=jchambers" | grep ASSOCIATION | awk '{print $4}' | uniq`)
-i=0
-x=1
-while [ $i -lt ${#my_array[@]} ]
+#read the credentials from the file
+credentials=$(cat ~/Downloads/credentials)
+
+#loop through each line of the credentials
+for line in $credentials
 do
-	echo $x ${my_array[$i]}
-	i=$(( $i + 1 ))
-	x=$(( $x + 1 ))
-	# Add logic to set the IP to a hostname var for easier reconnects.
+    #use a case statement to check if the line starts with "aws_access_key_id"
+    case $line in
+        "aws_access_key_id"*)
+            #export the access key
+            export AWS_ACCESS_KEY_ID=${line#*=}
+        ;;
+        #if the line starts with "aws_secret_access_key"
+        "aws_secret_access_key"*)
+            #export the secret access key
+            export AWS_SECRET_ACCESS_KEY=${line#*=}
+        ;;
+        #if the line starts with "aws_session_token"
+        "aws_session_token"*)
+            #export the session token
+            export AWS_SESSION_TOKEN=${line#*=}
+        ;;
+    esac
 done
 
-read -p 'Choose a server: ' server
-re='^[0-9]+$'
-
-if ! [[ $server =~ $re ]] ; then
-   echo "error: Not a number" >&2; exit 1
+#check if the AWS access and secret key environment variables are set
+if [ -z "$AWS_ACCESS_KEY_ID" ] || [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
+    echo "AWS access and secret keys not found!"
+    exit 1
 fi
-ssh -i $HOME/.ssh/joeskeyaws_ohio.pem -o "StrictHostKeyChecking no" ubuntu@`echo ${my_array[$server - 1]}`
 
-# Testing stuff
-#echo ${#my_array[@]}
-#echo ${my_array[*]}
+export key_path=/Users/chambj/.ssh/joeskeyaws_ohio.pem
+
+#check if the key_path environment variable is set
+if [ -z "$key_path" ]; then
+    echo "Key path not found!"
+    exit 1
+fi
+
+# List EC2 instances with key name containing jchambers
+aws ec2 describe-instances --filters Name=tag:Name,Values='*jchambers*' \
+--query 'Reservations[*].Instances[*].[InstanceId, PrivateIpAddress, PublicIpAddress, Tags[?Key==`Name`].Value[]]'
+
+# Prompt user to ssh with chosen key
+read -p "Enter the ID of the instance you want to SSH into: " instance_id
+
+#function to ssh into the instance
+function ssh_instance {
+    # Check if instance is running
+    instance_status=$(aws ec2 describe-instances --instance-ids $instance_id --query "Reservations[*].Instances[*].State.Name" --output text)
+
+    if [ "$instance_status" != "running" ]; then
+      echo "Instance is not running. Starting instance..."
+      aws ec2 start-instances --instance-ids $instance_id
+      echo "Waiting for instance to start..."
+      aws ec2 wait instance-running --instance-ids $instance_id
+    fi
+
+    sleep 5
+
+    echo "SSHing into instance $instance_id with key $key_path..."
+    ssh -i $key_path -o "StrictHostKeyChecking no" ubuntu@$(aws ec2 describe-instances --instance-id $instance_id --query "Reservations[*].Instances[*].PublicIpAddress" --output text)
+
+    if [ $? -ne 0 ]; then
+      echo "Failed to ssh as ubuntu. Trying as ec2-user..."
+      ssh -i $key_path -o "StrictHostKeyChecking no" ec2-user@$(aws ec2 describe-instances --instance-id $instance_id --query "Reservations[*].Instances[*].PublicIpAddress" --output text)
+      if [ $? -ne 0 ]; then
+        echo "Failed to ssh as ec2-user. Exiting..."
+        exit 1
+      fi
+    fi
+}
+
+#call the ssh_instance function
+ssh_instance
